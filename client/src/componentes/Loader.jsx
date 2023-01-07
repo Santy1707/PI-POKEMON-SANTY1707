d638f371c61e63894beebabfd8e1b11939d846c7
Santiago <98236588+Santy1707@users.noreply.github.com>
2023-01-06 19:49:34 -0500

    Agregando error 404 y Loader en el front

---
+++ client/src/componentes/Loader.jsx
@@ -0,0 +1,14 @@
+import './Loader.css'
+import React from 'react';
+
+
+const Loader = () => {
+    return (
+        <div>
+            <div className="loader">
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
