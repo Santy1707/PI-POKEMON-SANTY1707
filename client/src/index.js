2777aca7e716f130bd3db71a6d0235a5262d3de4
Santiago <98236588+Santy1707@users.noreply.github.com>
2023-01-09 14:30:09 -0500

    Icono y nombre la página establecidos

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://pi-pokemon-santy1707-production.up.railway.app/'
+axios.defaults.baseURL = 'https://pi-pokemon-santy1707-production.up.railway.app'
 
 ReactDOM.render(
   <Provider store={store}>
